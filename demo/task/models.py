f930c39abec534974677fb231e0c08ea99932584
plamen <looopooo45@gmail.com>
2024-11-12 12:12:27 +0200

    todolist

--- demo/task/models.py
+++ demo/task/models.py
@@ -4,14 +4,16 @@ from django.db import models
 
 from account.models import User
 from project.models import Project
+from todolist.models import Todolist
 
 
-class Todolist(models.Model):    
+class Task(models.Model):    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    project = models.ForeignKey(Project, related_name='todolists', on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE)
+    todolist=models.ForeignKey(Todolist,related_name='task', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    created_by = models.ForeignKey(User, related_name='todolists', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='tasks', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
