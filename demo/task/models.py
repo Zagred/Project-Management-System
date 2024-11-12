87c92a33b174b7ab435117b48c4a71e2aa14d103
plamen <looopooo45@gmail.com>
2024-11-12 12:53:48 +0200

    task edit/mark as done/delete

--- demo/task/models.py
+++ demo/task/models.py
@@ -10,9 +10,10 @@ from todolist.models import Todolist
 class Task(models.Model):    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE)
-    todolist=models.ForeignKey(Todolist,related_name='task', on_delete=models.CASCADE)
+    todolist = models.ForeignKey(Todolist, related_name='tasks', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    is_done = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='tasks', on_delete=models.CASCADE)
 
     def __str__(self):
