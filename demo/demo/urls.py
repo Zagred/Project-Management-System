3dc0a12dd05faaf2ef51b7725049d88452dd55d6
plamen <looopooo45@gmail.com>
2024-11-14 20:12:22 +0200

    added file upload/delete, note create/edit/delete

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +12,4 @@ urlpatterns = [
     path('projects/', include('project.urls')),
     path('projects/<uuid:project_id>/', include('todolist.urls')),
     path('projects/<uuid:project_id>/<uuid:todolist_id>/', include('task.urls')),
-]
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
